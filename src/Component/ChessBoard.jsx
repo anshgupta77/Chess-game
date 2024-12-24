4dbf3b27a7c230a640b67eaabff93b190d980915
Dev-using-Ansh <kuamrguptaansh0@gmail.com>
2024-12-24 17:49:32 +0530

    Bug free Code

--- src/Component/ChessBoard.jsx
+++ src/Component/ChessBoard.jsx
@@ -1,112 +1,84 @@
 
 import { useState } from "react";
-import rook_black from "../assets/rook-black.png";
-import knight_black from "../assets/knight-black.png";
-import queen_black from "../assets/queen-black.png";
-import king_black from "../assets/king-black.png";
-import bishop_black from "../assets/bishop-black.png";
-import pawn_black from "../assets/pawn-black.png";
-import bishop_white from "../assets/bishop-white.png";
-import king_white from "../assets/king-white.png";
-import knight_white from "../assets/knight-white.png";
-import pawn_white from "../assets/pawn-white.png";
-import rook_white from "../assets/rook-white.png";
-import queen_white from "../assets/queen-white.png";
-
+import { initialState } from "../service/initialState";
 const ChessBoard = () => {
-    // const iniitailState = [
-    //     [{src: rook_black, color: "black"}, knight_black, bishop_black, queen_black, king_black, bishop_black, knight_black, rook_black],
-    //     [pawn_black, pawn_black, pawn_black, pawn_black, pawn_black, pawn_black, pawn_black, pawn_black],
-    //     ['', '', '', '', '', '', '', ''],
-    //     ['', '', '', '', '', '', '', ''],
-    //     ['', '', '', '', '', '', '', ''],
-    //     ['', '', '', '', '', '', '', ''],
-    //     [pawn_white, pawn_white, pawn_white, pawn_white, pawn_white, pawn_white, pawn_white, pawn_white],
-    //     [rook_white, knight_white, bishop_white, queen_white, king_white, bishop_white, knight_white, rook_white]
-    // ]
-    const initialState = [
-        [
-            {src: rook_black, color: "black"},
-            {src: knight_black, color: "black"},
-            {src: bishop_black, color: "black"},
-            {src: queen_black, color: "black"},
-            {src: king_black, color: "black"},
-            {src: bishop_black, color: "black"},
-            {src: knight_black, color: "black"},
-            {src: rook_black, color: "black"}
-        ],
-        [
-            {src: pawn_black, color: "black"},
-            {src: pawn_black, color: "black"},
-            {src: pawn_black, color: "black"},
-            {src: pawn_black, color: "black"},
-            {src: pawn_black, color: "black"},
-            {src: pawn_black, color: "black"},
-            {src: pawn_black, color: "black"},
-            {src: pawn_black, color: "black"}
-        ],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        [
-            {src: pawn_white, color: "white"},
-            {src: pawn_white, color: "white"},
-            {src: pawn_white, color: "white"},
-            {src: pawn_white, color: "white"},
-            {src: pawn_white, color: "white"},
-            {src: pawn_white, color: "white"},
-            {src: pawn_white, color: "white"},
-            {src: pawn_white, color: "white"}
-        ],
-        [
-            {src: rook_white, color: "white"},
-            {src: knight_white, color: "white"},
-            {src: bishop_white, color: "white"},
-            {src: queen_white, color: "white"},
-            {src: king_white, color: "white"},
-            {src: bishop_white, color: "white"},
-            {src: knight_white, color: "white"},
-            {src: rook_white, color: "white"}
-        ]
-    ];
-    
-   
-
-    const [boardImages, setBoardImages] = useState(initialState);
+    const [boardObject, setBoardObject] = useState(initialState);
     const [selectedPiece , setSelectedPiece] = useState(null);
     const [turn, setTurn] = useState("white");
-
+    console.log("Selectedpiece" , selectedPiece);
+    console.log("BoardState", boardObject);
     function handleCellClick(i, j) {
-        // console.log(boardImages);
-        const piece = boardImages[i][j];
+        const piece = boardObject[i][j];
         if(!selectedPiece){
-            if(piece !== ""){
-                // const imageCoordinates = {i,j};
                 setSelectedPiece({i,j});
-            }
+                // getNamePieceSelected(selectedPiece);
         }else{
-            console.log("Turn and Selected Piece",turn,selectedPiece);
-            if(isValidMove(selectedPiece.i, selectedPiece.j)){
-                const newBoardImages = [...boardImages];
-                newBoardImages[i][j] = boardImages[selectedPiece.i][selectedPiece.j];
-                newBoardImages[selectedPiece.i][selectedPiece.j] = "";
-                setBoardImages(newBoardImages);
-                setTurn(prev => prev === "white" ? "black" : "white");
+                if((piece.src !== null && piece.color !== turn) || (piece.src === null)){
+                    console.log("Turn and Selected Piece",turn,selectedPiece);
+                    if(isValidMove(selectedPiece.i, selectedPiece.j)){
+                        const newBoardObject = [...boardObject];
+                        newBoardObject[i][j].src = boardObject[selectedPiece.i][selectedPiece.j].src;
+                        newBoardObject[selectedPiece.i][selectedPiece.j].src = "";
+                        setBoardObject(newBoardObject);
+                        setTurn(prev => prev === "white" ? "black" : "white");
+                    }
+                    clearDropable();
+                    setSelectedPiece(null);
+                }
             }
-            setSelectedPiece(null);
         }
-
-    }
-
     function isValidMove(i, j){
-        const pieceColor = boardImages[i][j].color;
+        const pieceColor = boardObject[i][j].color;
         console.log(pieceColor, turn);
         if(pieceColor === turn){
             return true;
         }
         return false;
     }
+
+    function handleRook(piece){
+        const newBoardObject = [...boardObject];
+        for(let i=0; i<8; i++){
+            for(let j=0; j<8; j++){
+
+                if(newBoardObject[i][j].src === ""){
+                    break;
+                }
+                newBoardObject[i][j].dropable = true;
+            }
+        }
+        for(let i=0; i<8; i++){
+            for(let j=0; j<8; j++){
+                if(newBoardObject[j][i].src === ""){
+                    break;
+                }
+                newBoardObject[j][i].dropable = true;
+            }
+        }
+        setBoardObject(newBoardObject);
+    
+    }
+    function getNamePieceSelected(piece){
+        const {i, j} = piece;
+        const pieceName = boardObject[i][j].name;
+        switch (pieceName){
+            case "rook" : handleRook(piece);
+            default: console.log("Invalid input");
+        }
+    }
+    function clearDropable(){
+        if(initialState === boardObject) return;
+        const newBoardObject = [...boardObject];
+        for(let i=0; i<8; i++){
+            for(let j=0; j<8; j++){
+                newBoardObject[i][j].dropable = false;
+            }
+        }
+        setBoardObject(newBoardObject);
+    }
+    // if(selectedPiece){
+    //     getNamePieceSelected(selectedPiece);
+    // }
     
     function chessBoard() {
         const board = [];
@@ -121,10 +93,10 @@ const ChessBoard = () => {
                             (i + j) % 2 === 0 ? "bg-yellow-200" : "bg-gray-600"
                         }`}
                     >
-                        {boardImages[i][j] && (
+                        {boardObject[i][j].src !== "" && (
                             <img
-                                src={`${boardImages[i][j].src}`}
-                                alt={boardImages[i][j].src}
+                                src={`${boardObject[i][j].src}`}
+                                alt={boardObject[i][j].src}
                                 className="w-8 h-8"
                             />
                         )}
@@ -134,6 +106,7 @@ const ChessBoard = () => {
         }
         return board;
     }
+    
 
     return (
         <div className="h-[100vh] flex justify-center items-center bg-slate-200">
---
+++ src/service/initialState.jsx
@@ -0,0 +1,95 @@
+import rook_black from "../assets/rook-black.png";
+import knight_black from "../assets/knight-black.png";
+import queen_black from "../assets/queen-black.png";
+import king_black from "../assets/king-black.png";
+import bishop_black from "../assets/bishop-black.png";
+import pawn_black from "../assets/pawn-black.png";
+import bishop_white from "../assets/bishop-white.png";
+import king_white from "../assets/king-white.png";
+import knight_white from "../assets/knight-white.png";
+import pawn_white from "../assets/pawn-white.png";
+import rook_white from "../assets/rook-white.png";
+import queen_white from "../assets/queen-white.png";
+
+export const initialState = [
+    [
+        { src: rook_black, color: "black", name: "rook", dropable: false },
+        { src: knight_black, color: "black", name: "knight" , dropable: false},
+        { src: bishop_black, color: "black", name: "bishop" , dropable: false },
+        { src: queen_black, color: "black", name: "queen"  , dropable: false },
+        { src: king_black, color: "black", name: "king"  , dropable: false },
+        { src: bishop_black, color: "black", name: "bishop"  , dropable: false },
+        { src: knight_black, color: "black", name: "knight"  , dropable: false },
+        { src: rook_black, color: "black", name: "rook"  , dropable: false }
+    ],
+    [
+        { src: pawn_black, color: "black", name: "pawn"  , dropable: false },
+        { src: pawn_black, color: "black", name: "pawn"  , dropable: false },
+        { src: pawn_black, color: "black", name: "pawn"  , dropable: false },
+        { src: pawn_black, color: "black", name: "pawn"  , dropable: false },
+        { src: pawn_black, color: "black", name: "pawn"  , dropable: false },
+        { src: pawn_black, color: "black", name: "pawn"  , dropable: false },
+        { src: pawn_black, color: "black", name: "pawn"  , dropable: false },
+        { src: pawn_black, color: "black", name: "pawn"  , dropable: false }
+    ],
+    [
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false },  
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false },
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+    ],
+    [
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false },  
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false },
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+    ],
+    [
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false },  
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false },
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+    ],
+    [
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false },  
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false },
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+        {src: "", color: "", name: "", dropable: false }, 
+    ],
+    [
+        { src: pawn_white, color: "white", name: "pawn" ,dropable: false},
+        { src: pawn_white, color: "white", name: "pawn" ,dropable: false},
+        { src: pawn_white, color: "white", name: "pawn" ,dropable: false},
+        { src: pawn_white, color: "white", name: "pawn" ,dropable: false},
+        { src: pawn_white, color: "white", name: "pawn" ,dropable: false},
+        { src: pawn_white, color: "white", name: "pawn" ,dropable: false},
+        { src: pawn_white, color: "white", name: "pawn" ,dropable: false},
+        { src: pawn_white, color: "white", name: "pawn" ,dropable: false}
+    ],
+    [
+        { src: rook_white, color: "white", name: "rook" ,dropable: false},
+        { src: knight_white, color: "white", name: "knight",dropable: false },
+        { src: bishop_white, color: "white", name: "bishop",dropable: false },
+        { src: queen_white, color: "white", name: "queen" ,dropable: false},
+        { src: king_white, color: "white", name: "king",dropable: false },
+        { src: bishop_white, color: "white", name: "bishop",dropable: false },
+        { src: knight_white, color: "white", name: "knight" ,dropable: false},
+        { src: rook_white, color: "white", name: "rook",dropable: false }
+    ]
+];
